a2f103255d3e51a9dbfeeff814bdeb01e29808e3
enfusq <aivars.trumars@gmail.com>
2025-04-02 14:24:13 +0300

    Fixed

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -9,6 +9,6 @@ class CommentPolicy
 {
     public function delete(User $user, Comment $comment)
     {
-        return $user->id === $comment->user()->user_id;
+        return $user->id === $comment->user_id;
     }
 }
